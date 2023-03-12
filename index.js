8231c8779fd722aae7c8fed03545768b80578ed1
alvinkenyagah <alvin47k@gmail.com>
2023-03-12 12:35:21 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 main.remove();
-
-let newHeader = document.createElement('h1');
-newHeader.id='victory';
+let newHeader =document.createElement('h1');
+newHeader.id='victory'
 newHeader.innerHTML="Alvin is the champion";
+
+
+// function edit() {
+//   main.remove();
+
+//   let newHeader = document.createElement("h1");
+//   newHeader.id = "victory";
+//   newHeader.innerHTML = "Alvin is the champion";
+//   document.body.appendChild(newHeader);
+// }
+
+// edit();
